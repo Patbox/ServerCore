48cefcda3f4742181bd9ded820c33cdb86c9c700
Patbox <39821509+Patbox@users.noreply.github.com>
2020-12-29 21:45:32 +0100

    Mini fix

--- interfaces/plugin.ts
+++ interfaces/plugin.ts
@@ -2,7 +2,7 @@ export interface ICorePlugin {
 	name: string;
 	version: string;
 	supportedAPI: string;
-	game: string | '*';
-	supportedGameAPI: string | '*';
+	game: string;
+	supportedGameAPI: string;
 	[index: string]: any; 
 }
--- values.js
+++ values.js
@@ -1,4 +1,4 @@
 "use strict";
 exports.__esModule = true;
 exports.version = void 0;
-exports.version = '0.1.1';
+exports.version = '0.1.2';
