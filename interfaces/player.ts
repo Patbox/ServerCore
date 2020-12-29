127522b43bc746b5751121a4094ef8ed635a06e2
Patbox <39821509+Patbox@users.noreply.github.com>
2020-12-29 19:26:20 +0100

    Forgot to rename this

--- interfaces/player.ts
+++ interfaces/player.ts
@@ -4,17 +4,17 @@ import { Position } from './types';
 import { CoreWorld } from './world';
 
 export interface CorePlayerManager {
-	players: { [index: string]: IPlayer };
+	players: { [index: string]: CorePlayer };
 	banlist: { [index: string]: string };
 	ipbanlist: { [index: string]: string };
 	cache: { [index: string]: { [index: string]: string } };
 
-	create(id: string, data: any, socket: any): IPlayer;
+	create(id: string, data: any, socket: any): CorePlayer;
 	read(id: string): object | null;
 	exist(id: string): boolean;
 	save(id: string, data: object);
-	get(id: string): IPlayer | null;
-	getAll(): { [index: string]: IPlayer };
+	get(id: string): CorePlayer | null;
+	getAll(): { [index: string]: CorePlayer };
 	isBanned(id: string): boolean;
 	isIPBanned(ip: string): boolean;
 	getBanReason(id: string): string;
@@ -23,7 +23,7 @@ export interface CorePlayerManager {
 	banIP(ip: string, reason?: string): void;
 }
 
-export interface IPlayer {
+export interface CorePlayer {
 	readonly id: string;
 	readonly nickname: string;
 	readonly ipAddress: string;
